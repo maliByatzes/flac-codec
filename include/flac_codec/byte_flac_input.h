1a09e0a3753e1e9f7b24bbcee60cf12e291617b1
maliByatzes <malib2027@gmail.com>
2025-10-23 01:56:56 +0200

    implement byte flac input class

---
+++ include/flac_codec/byte_flac_input.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <flac_codec/flac_low_level_input.h>
+#include <optional>
+#include <vector>
+
+namespace flac {
+
+class ByteFlacInput : public FlacLowLevelInput
+{
+private:
+  std::vector<uint8_t> m_data;
+  size_t m_offset;
+
+public:
+  explicit ByteFlacInput(std::vector<uint8_t> bytes);
+
+  [[nodiscard]] std::optional<size_t> get_length() const override;
+  void seek_to(size_t pos) override;
+  std::optional<uint64_t> read_underlying(std::vector<uint8_t> &buf, size_t off, size_t len) override;
+  void close() override;
+};
+
+}// namespace flac
---
+++ src/byte_flac_input.cpp
@@ -0,0 +1,44 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdint>
+#include <flac_codec/byte_flac_input.h>
+#include <flac_codec/flac_low_level_input.h>
+#include <optional>
+#include <stdexcept>
+#include <utility>
+#include <vector>
+
+namespace flac {
+
+ByteFlacInput::ByteFlacInput(std::vector<uint8_t> bytes) : m_data(std::move(bytes)), m_offset(0) {};
+
+std::optional<size_t> ByteFlacInput::get_length() const { return m_data.size(); }
+
+void ByteFlacInput::seek_to(size_t pos)
+{
+  m_offset = pos;
+  position_changed(pos);
+}
+
+std::optional<uint64_t> ByteFlacInput::read_underlying(std::vector<uint8_t> &buf, size_t off, size_t len)
+{
+  if (off > buf.size() || len > buf.size() - off) { throw std::invalid_argument("Buffer is out of bounds."); }
+
+  auto min = std::min(m_data.size() - m_offset, len);
+  if (min == 0) { return std::nullopt; }
+
+  std::copy(m_data.begin() + long(m_offset), m_data.begin() + long(m_offset) + long(min), buf.begin() + long(off));
+
+  m_offset += min;
+  return min;
+}
+
+void ByteFlacInput::close()
+{
+  // NOTE: eeehehehe
+  if (!m_data.empty()) {
+    m_data.clear();
+    FlacLowLevelInput::close();
+  }
+}
+}// namespace flac
