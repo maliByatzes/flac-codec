39d7846ebf0a69aaee7dbb9bb22ca2060b44e6b0
maliByatzes <malib2027@gmail.com>
2025-10-22 14:56:16 +0200

    change types and function defintions

--- include/flac_codec/flac_low_level_input.h
+++ include/flac_codec/flac_low_level_input.h
@@ -1,6 +1,9 @@
 #pragma once
 
+#include <cstddef>
 #include <cstdint>
+#include <optional>
+#include <variant>
 #include <vector>
 
 namespace flac {
@@ -10,20 +13,20 @@ class IFlacLowLevelInput// NOLINT
 public:
   virtual ~IFlacLowLevelInput() = default;
 
-  [[nodiscard]] virtual long get_length() const = 0;
-  [[nodiscard]] virtual long get_position() const = 0;
-  [[nodiscard]] virtual int get_bit_position() const = 0;
-  virtual void seek_to(long pos) = 0;
+  [[nodiscard]] virtual std::optional<size_t> get_length() const = 0;
+  [[nodiscard]] virtual std::optional<size_t> get_position() const = 0;
+  [[nodiscard]] virtual std::optional<size_t> get_bit_position() const = 0;
+  virtual void seek_to(size_t pos) = 0;
 
-  virtual int read_uint(int nnn) = 0;
-  virtual int read_signed_int(int nnn) = 0;
-  virtual void read_rice_signed_ints(int param, std::vector<long> &result, int start, int end) = 0;
-  [[nodiscard]] virtual int read_byte() = 0;
+  virtual std::variant<uint32_t, int32_t> read_uint(size_t num_of_bits) = 0;
+  virtual int32_t read_signed_int(size_t num_of_bits) = 0;
+  virtual void read_rice_signed_ints(int param, std::vector<int64_t> &result, size_t start, size_t end) = 0;
+  [[nodiscard]] virtual std::optional<uint8_t> read_byte() = 0;
   virtual void read_fully(std::vector<uint8_t> &bytes) = 0;
 
   virtual void reset_crcs() = 0;
-  [[nodiscard]] virtual int get_crc8() = 0;
-  [[nodiscard]] virtual int get_crc16() = 0;
+  [[nodiscard]] virtual uint8_t get_crc8() = 0;
+  [[nodiscard]] virtual uint16_t get_crc16() = 0;
 
   virtual void close() = 0;
 };
@@ -31,54 +34,54 @@ public:
 class FlacLowLevelInput : public IFlacLowLevelInput
 {
 private:
-  long m_byte_buffer_start_pos;
+  size_t m_byte_buffer_start_pos;
   std::vector<uint8_t> m_byte_buffer;
-  int m_byte_buffer_len;
-  int m_byte_buffer_index;
+  size_t m_byte_buffer_len;
+  size_t m_byte_buffer_index;
 
-  long m_bit_buffer;
-  int m_bit_buffer_len;
+  uint64_t m_bit_buffer;
+  size_t m_bit_buffer_len;
 
-  int m_crc8;
-  int m_crc16;
-  int m_crc_start_index;
+  uint8_t m_crc8;
+  uint16_t m_crc16;
+  size_t m_crc_start_index;
 
   void check_byte_aligned() const;
   void fill_bit_buffer();
-  int read_underlying();
-  void update_crcs(int unused_trailing_bytes);
+  std::optional<uint8_t> read_underlying();
+  void update_crcs(size_t unused_trailing_bytes);
 
 public:
   FlacLowLevelInput();
 
-  [[nodiscard]] long get_position() const override;
-  [[nodiscard]] int get_bit_position() const override;
+  [[nodiscard]] std::optional<size_t> get_position() const override;
+  [[nodiscard]] std::optional<size_t> get_bit_position() const override;
 
-  int read_uint(int nnn) override;
-  int read_signed_int(int nnn) override;
-  void read_rice_signed_ints(int param, std::vector<long> &result, int start, int end) override;
-  int read_byte() override;
+  std::variant<uint32_t, int32_t> read_uint(size_t num_of_bits) override;
+  int32_t read_signed_int(size_t num_of_bits) override;
+  void read_rice_signed_ints(int param, std::vector<int64_t> &result, size_t start, size_t end) override;
+  std::optional<uint8_t> read_byte() override;
   void read_fully(std::vector<uint8_t> &bytes) override;
   void reset_crcs() override;
-  [[nodiscard]] int get_crc8() override;
-  [[nodiscard]] int get_crc16() override;
+  [[nodiscard]] uint8_t get_crc8() override;
+  [[nodiscard]] uint16_t get_crc16() override;
   void close() override;
 
 protected:
-  void position_changed(long pos);
-  virtual int read_underlying(std::vector<uint8_t> &buf, int off, int len) = 0;
+  void position_changed(size_t pos);
+  virtual std::optional<uint64_t> read_underlying(std::vector<uint8_t> &buf, size_t off, size_t len) = 0;
 
 private:
-  static const int RICE_DECODING_TABLE_BITS = 13;
-  static const int RICE_DECODING_TABLE_MASK = (1U << RICE_DECODING_TABLE_BITS) - 1U;// NOLINT
+  static const size_t RICE_DECODING_TABLE_BITS = 13;
+  static const size_t RICE_DECODING_TABLE_MASK = (1U << RICE_DECODING_TABLE_BITS) - 1U;
   static std::vector<std::vector<uint8_t>> RICE_DECODING_CONSUMED_TABLES;
-  static std::vector<std::vector<int>> RICE_DECODING_VALUE_TABLES;
-  static const int RICE_DECODING_CHUNK = 4;
+  static std::vector<std::vector<uint32_t>> RICE_DECODING_VALUE_TABLES;
+  static const size_t RICE_DECODING_CHUNK = 4;
 
   static void initialize_tables();
 
   static std::vector<uint8_t> CRC8_TABLE;
-  static std::vector<char> CRC16_TABLE;
+  static std::vector<uint16_t> CRC16_TABLE;
 
   static void initialize_crcs();
 };
