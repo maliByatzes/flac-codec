8a8ecd873dbaa48525e508ffacfff309eb7f5945
maliByatzes <malib2027@gmail.com>
2025-10-22 13:10:36 +0200

    continue implementing flac_low_leve_input class

--- include/flac_codec/flac_low_level_input.h
+++ include/flac_codec/flac_low_level_input.h
@@ -18,12 +18,12 @@ public:
   virtual int read_uint(int nnn) = 0;
   virtual int read_signed_int(int nnn) = 0;
   virtual void read_rice_signed_ints(int param, std::vector<long> &result, int start, int end) = 0;
-  [[nodiscard]] virtual int read_byte() const = 0;
-  virtual void read_fully(const std::vector<uint8_t> &bytes) const = 0;
+  [[nodiscard]] virtual int read_byte() = 0;
+  virtual void read_fully(std::vector<uint8_t> &bytes) = 0;
 
   virtual void reset_crcs() = 0;
-  [[nodiscard]] virtual int get_crc8() const = 0;
-  [[nodiscard]] virtual int get_crc16() const = 0;
+  [[nodiscard]] virtual int get_crc8() = 0;
+  [[nodiscard]] virtual int get_crc16() = 0;
 
   virtual void close() = 0;
 };
@@ -43,22 +43,39 @@ private:
   int m_crc16;
   int m_crc_start_index;
 
+  void check_byte_aligned() const;
+  void fill_bit_buffer();
+  int read_underlying();
+  void update_crcs(int unused_trailing_bytes);
+
 public:
   FlacLowLevelInput();
 
   [[nodiscard]] long get_position() const override;
   [[nodiscard]] int get_bit_position() const override;
 
-protected:
-  void position_changed(long pos);
-
-private:
-  void check_byte_aligned() const;
-
-public:
   int read_uint(int nnn) override;
   int read_signed_int(int nnn) override;
   void read_rice_signed_ints(int param, std::vector<long> &result, int start, int end) override;
+  int read_byte() override;
+  void read_fully(std::vector<uint8_t> &bytes) override;
+  void reset_crcs() override;
+  [[nodiscard]] int get_crc8() override;
+  [[nodiscard]] int get_crc16() override;
+  void close() override;
+
+protected:
+  void position_changed(long pos);
+  virtual int read_underlying(std::vector<uint8_t> &buf, int off, int len) = 0;
+
+private:
+  static const int RICE_DECODING_TABLE_BITS = 13;
+  static const int RICE_DECODING_TABLE_MASK = (1U << RICE_DECODING_TABLE_BITS) - 1U;// NOLINT
+  static std::vector<std::vector<uint8_t>> RICE_DECODING_CONSUMED_TABLES;
+  static std::vector<std::vector<int>> RICE_DECODING_VALUE_TABLES;
+  static const int RICE_DECODING_CHUNK = 4;
+
+  static void initialize();
 };
 
 }// namespace flac
--- src/flac_low_level_input.cpp
+++ src/flac_low_level_input.cpp
@@ -71,5 +71,158 @@ void FlacLowLevelInput::read_rice_signed_ints(int param, std::vector<long> &resu
   if (param < 0 || param > 31) { throw std::invalid_argument("Invalid argument"); }
 
   long unary_limit = 1L << (53 - param);// NOLINT
+
+  std::vector<uint8_t> consume_table = RICE_DECODING_CONSUMED_TABLES.at(param);
+  std::vector<int> value_table = RICE_DECODING_VALUE_TABLES.at(param);
+
+  while (true) {
+    while (start <= end - RICE_DECODING_CHUNK) {
+      if (m_bit_buffer_len < RICE_DECODING_CHUNK * RICE_DECODING_TABLE_BITS) {
+        if (m_byte_buffer_index <= m_byte_buffer_len - 8) {
+          fill_bit_buffer();
+        } else {
+          break;
+        }
+      }
+
+      for (int i = 0; i < RICE_DECODING_CHUNK; i++, start++) {
+        int extracted_bits =
+          static_cast<int>(m_bit_buffer >> (m_bit_buffer_len - RICE_DECODING_TABLE_BITS)) & RICE_DECODING_TABLE_MASK;
+        int consumed = static_cast<int>(consume_table.at(static_cast<size_t>(extracted_bits)));
+        if (consumed == 0) { goto middle; }
+        m_bit_buffer_len -= consumed;
+        result.at(size_t(start)) = value_table.at(size_t(extracted_bits));
+      }
+    }
+
+  middle:
+    if (start >= end) { break; }
+    long val = 0;
+    while (read_uint(1) == 0) {
+      if (val >= unary_limit) { throw std::runtime_error("Residual value is too large"); }
+      val++;
+    }
+    // NOLINTBEGIN
+    val = (val << param) | read_uint(param);// NOLINT
+    assert((val >> 53) == 0);
+    val = (val >> 1) ^ -(val & 1);
+    assert((val >> 52) == 0 || (val >> 52) == -1);
+    result.at(size_t(start)) = val;
+    start++;
+    // NOLINTEND
+  }
 }
+
+void FlacLowLevelInput::fill_bit_buffer()
+{
+  int i = m_byte_buffer_index;// NOLINT
+  int n = std::min((64 - m_bit_buffer_len) >> 3, m_byte_buffer_len - i);// NOLINT
+  std::vector<uint8_t> bytes = m_byte_buffer;
+
+  if (n > 0) {
+    for (int j = 0; j < n; j++, i++) { m_bit_buffer = (m_bit_buffer << 8) | (bytes.at(size_t(i)) & 0xFF); }// NOLINT
+    m_bit_buffer_len += n << 3;// NOLINT
+  } else if (m_bit_buffer_len <= 56) {
+    int temp = read_underlying();
+    if (temp == -1) { throw std::runtime_error("Reached EOF"); }
+    m_bit_buffer = (m_bit_buffer << 8) | temp;// NOLINT
+    m_bit_buffer_len += 8;
+  }
+
+  assert(8 <= m_bit_buffer_len && m_bit_buffer_len <= 64);
+  m_byte_buffer_index += n;
+}
+
+int FlacLowLevelInput::read_byte()
+{
+  check_byte_aligned();
+  if (m_bit_buffer_len >= 8) {
+    return read_uint(8);
+  } else {
+    assert(m_bit_buffer_len == 0);
+    return read_underlying();
+  }
+}
+
+void FlacLowLevelInput::read_fully(std::vector<uint8_t> &bytes)
+{
+  if (bytes.data() == nullptr) { throw std::invalid_argument("Bytes argument is invalid"); }
+  check_byte_aligned();
+  for (auto &byte : bytes) { byte = static_cast<uint8_t>(read_uint(8)); }
+}
+
+int FlacLowLevelInput::read_underlying()
+{
+  if (m_byte_buffer_index >= m_byte_buffer_len) {
+    if (m_byte_buffer_len == -1) { return -1; }
+    m_byte_buffer_start_pos += m_byte_buffer_len;
+    update_crcs(0);
+    m_byte_buffer_len = read_underlying(m_byte_buffer, 0, int(m_byte_buffer.size()));
+    m_crc_start_index = 0;
+    if (m_byte_buffer_len <= 0) { return -1; }
+    m_byte_buffer_index = 0;
+  }
+
+  assert(m_byte_buffer_index < m_byte_buffer_len);
+  int temp = m_byte_buffer.at(size_t(m_byte_buffer_index)) & 0xFF;// NOLINT
+  m_byte_buffer_index++;
+  return temp;
+}
+
+void FlacLowLevelInput::reset_crcs()
+{
+  check_byte_aligned();
+  m_crc_start_index = m_byte_buffer_index - m_bit_buffer_len / 8;
+  m_crc8 = 0;
+  m_crc16 = 0;
+}
+
+int FlacLowLevelInput::get_crc8()
+{
+  check_byte_aligned();
+  update_crcs(m_bit_buffer_len / 8);
+  if ((m_crc8 >> 8) != 0) { throw std::runtime_error("Assertion error"); }// NOLINT
+  return m_crc8;
+}
+
+int FlacLowLevelInput::get_crc16()
+{
+  check_byte_aligned();
+  update_crcs(m_bit_buffer_len / 8);
+  if ((m_crc16 >> 16) != 0) { throw std::runtime_error("Assertion error"); }// NOLINT
+  return m_crc16;
+}
+
+void FlacLowLevelInput::update_crcs(int unused_trailing_bytes)
+{
+  int end = m_byte_buffer_index - unused_trailing_bytes;
+  for (int i = m_crc_start_index; i < end; ++i) {
+    int b = m_byte_buffer.at(size_t(i)) & 0xFF;// NOLINT
+    m_crc8 = CRC8_TABLE.at(m_crc8 ^ b) & 0xFF;// NOLINT
+    m_crc16 = CRC16_TABLE.at((m_crc16 >> 8) & b)((m_crc16 & 0xFF) << 8);// NOLINT
+    assert((m_crc8 >> 8) == 0);// NOLINT
+    assert((m_crc16 >> 16) == 0);// NOLINT
+  }
+  m_crc_start_index = end;
+}
+
+void FlacLowLevelInput::close()
+{
+  m_byte_buffer.clear();
+  m_byte_buffer_len = -1;
+  m_byte_buffer_index = -1;
+  m_bit_buffer = 0;
+  m_bit_buffer_len = 0;
+  m_crc8 = -1;
+  m_crc16 = -1;
+  m_crc_start_index = -1;
+}
+
+std::vector<std::vector<uint8_t>> FlacLowLevelInput::RICE_DECODING_CONSUMED_TABLES(31,
+  std::vector<uint8_t>(1U << FlacLowLevelInput::RICE_DECODING_TABLE_BITS));
+
+std::vector<std::vector<int>> FlacLowLevelInput::RICE_DECODING_VALUE_TABLES(31,
+  std::vector<int>(1U << RICE_DECODING_TABLE_BITS));
+
+void FlacLowLevelInput::initialize() {}
 }// namespace flac
