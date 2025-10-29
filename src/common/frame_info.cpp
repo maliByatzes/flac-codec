cd8087c170898702ecbaf1294c5ed9b4dcc78e85
maliByatzes <malib2027@gmail.com>
2025-10-29 15:36:59 +0200

    add custom exception messages in frame info class

--- src/common/frame_info.cpp
+++ src/common/frame_info.cpp
@@ -2,9 +2,11 @@
 #include <cassert>
 #include <cstdint>
 #include <flac_codec/common/frame_info.h>
+#include <flac_codec/decode/data_format_exception.h>
 #include <flac_codec/decode/flac_low_level_input.h>
 #include <optional>
 #include <stdexcept>
+#include <string>
 #include <vector>
 
 namespace flac {
@@ -26,9 +28,9 @@ std::optional<FrameInfo> FrameInfo::read_frame(IFlacLowLevelInput &input)
   result.m_frame_size = std::nullopt;
 
   auto sync = static_cast<uint16_t>((temp << 6U) | static_cast<uint8_t>(input.read_uint(6)));// NOLINT
-  if (sync != 0x3FFE) { throw std::runtime_error("Sync code expected"); }
+  if (sync != 0x3FFE) { throw DataFormatException("Sync code expected"); }
 
-  if (input.read_uint(1) != 0) { throw std::runtime_error("Reserved bit"); }
+  if (input.read_uint(1) != 0) { throw DataFormatException("Reserved bit"); }
 
   auto blocking_strategy = static_cast<uint8_t>(input.read_uint(1));
   auto block_size_code = static_cast<uint8_t>(input.read_uint(4));
@@ -42,28 +44,28 @@ std::optional<FrameInfo> FrameInfo::read_frame(IFlacLowLevelInput &input)
   } else if (8 <= chan_asgn && chan_asgn <= 10) {
     result.m_num_channels = 2U;
   } else {
-    throw std::runtime_error("Reserved channel assignment");
+    throw DataFormatException("Reserved channel assignment");
   }
 
   result.m_bit_depth = decode_bit_depth(static_cast<uint8_t>(input.read_uint(3)));
-  if (input.read_uint(1) != 0) { throw std::runtime_error("Reserved bit"); }
+  if (input.read_uint(1) != 0) { throw DataFormatException("Reserved bit"); }
 
   auto position = read_utf8_integer(input).value_or(0);
-  if (static_cast<int>(blocking_strategy) != 0) {
-    if ((position >> 31U) != 0) { throw std::runtime_error("Frame index too large"); }
+  if (static_cast<int>(blocking_strategy) == 0) {
+    if ((position >> 31U) != 0) { throw DataFormatException("Frame index too large"); }
     result.m_frame_index = position;
     result.m_sample_offset = std::nullopt;
   } else if (static_cast<int>(blocking_strategy) == 1) {
     result.m_sample_offset = position;
     result.m_frame_index = std::nullopt;
   } else {
-    throw std::logic_error("Assertion error");
+    throw std::runtime_error("Assertion error");
   }
 
   result.m_block_size = decode_block_size(block_size_code, input);
   result.m_sample_rate = decode_sample_rate(sample_rate_code, input);
   auto computed_crc8 = input.get_crc8();
-  if (static_cast<uint8_t>(input.read_uint(8)) != computed_crc8) { throw std::runtime_error("CRC-8 mismatch"); }
+  if (static_cast<uint8_t>(input.read_uint(8)) != computed_crc8) { throw DataFormatException("CRC-8 mismatch"); }
 
   return result;
 }
@@ -71,21 +73,21 @@ std::optional<FrameInfo> FrameInfo::read_frame(IFlacLowLevelInput &input)
 std::optional<uint64_t> FrameInfo::read_utf8_integer(IFlacLowLevelInput &input)
 {
   auto head = static_cast<uint8_t>(input.read_uint(8));
-  auto num_leading1s = std::countl_one(head);
+  auto num_leading1s = std::countl_one(static_cast<uint32_t>(~(head << 24U)));// NOLINT
   assert(0 <= num_leading1s && num_leading1s <= 8);
   if (num_leading1s == 0) {
     return head;
   } else if (num_leading1s == 1 || num_leading1s == 8) {
-    throw std::runtime_error("Invalid UTF-8 coded number");
+    throw DataFormatException("Invalid UTF-8 coded number");
   } else {
     uint64_t result = head & (0x7FU >> static_cast<uint16_t>(num_leading1s));
     for (int i = 0; i < num_leading1s - 1; ++i) {
       auto temp = static_cast<uint8_t>(input.read_uint(8));
-      if ((temp & 0xC0U) != 0x80U) { throw std::runtime_error("Invalid UTF-8 coded number"); }
+      if ((temp & 0xC0U) != 0x80U) { throw DataFormatException("Invalid UTF-8 coded number"); }
       result = (result << 6U) | (temp & 0x3FU);
     }
 
-    if ((result >> 36U) != 0) { throw std::logic_error("Assertion error"); }
+    if ((result >> 36U) != 0) { throw std::runtime_error("Assertion error"); }
 
     return result;
   }
@@ -93,11 +95,14 @@ std::optional<uint64_t> FrameInfo::read_utf8_integer(IFlacLowLevelInput &input)
 
 uint32_t FrameInfo::decode_block_size(uint8_t code, IFlacLowLevelInput &input)
 {
-  if ((code >> 4U) != 0) { throw std::invalid_argument("Code argument is invalid"); }
+  if ((code >> 4U) != 0) {
+    const std::string msg{ "code= " + std::to_string(static_cast<int>(code)) + ", is an invalid argument" };
+    throw std::invalid_argument(msg);
+  }
 
   switch (static_cast<int>(code)) {
   case 0:
-    throw std::runtime_error("Reserved block size");
+    throw DataFormatException("Reserved block size");
   case 6: {
     auto val = input.read_uint(8) + 1;
     return static_cast<uint32_t>(val);
@@ -108,7 +113,7 @@ uint32_t FrameInfo::decode_block_size(uint8_t code, IFlacLowLevelInput &input)
   }
   default: {
     auto result = search_second(BLOCK_SIZE_CODES, code).value_or(0);
-    if (result < 1 || result > 65536) { throw std::logic_error("Assertion failed."); }
+    if (result < 1 || result > 65536) { throw DataFormatException("Assertion failed."); }
     return result;
   }
   }
@@ -116,7 +121,11 @@ uint32_t FrameInfo::decode_block_size(uint8_t code, IFlacLowLevelInput &input)
 
 std::optional<uint32_t> FrameInfo::decode_sample_rate(uint8_t code, IFlacLowLevelInput &input)
 {
-  if ((code >> 4U) != 0) { throw std::invalid_argument("Code argument is invalid"); }
+  if ((code >> 4U) != 0) {
+    const std::string msg{ "code= " + std::to_string(static_cast<int>(code))
+                           + ", argument is invalid ((code >> 4) != 0)" };
+    throw std::invalid_argument(msg);
+  }
 
   switch (static_cast<int>(code)) {
   case 0:
@@ -134,10 +143,10 @@ std::optional<uint32_t> FrameInfo::decode_sample_rate(uint8_t code, IFlacLowLeve
     return static_cast<uint32_t>(val);
   }
   case 15:
-    throw std::runtime_error("Invalid sample rate");
+    throw DataFormatException("Invalid sample rate");
   default: {
     auto result = search_second(SAMPLE_RATE_CODES, code);
-    if (result < 1 || result > 655350) { throw std::logic_error("Assertion failed"); }
+    if (result < 1 || result > 655350) { throw std::runtime_error("Assertion failed"); }
     return result;
   }
   }
@@ -146,13 +155,16 @@ std::optional<uint32_t> FrameInfo::decode_sample_rate(uint8_t code, IFlacLowLeve
 std::optional<uint16_t> FrameInfo::decode_bit_depth(uint8_t code)
 {
   if ((code >> 3U) != 0) {
-    throw std::invalid_argument("Code argument is invalid");
+    const std::string msg{ "code= " + std::to_string(static_cast<int>(code))
+                           + ", argument is invalid ((code >> 3) != 0)" };
+    throw std::invalid_argument(msg);
   } else if (static_cast<int>(code) == 0) {
     return std::nullopt;
   } else {
-    auto result = search_second(BIT_DEPTH_CODES, code);
-    if (result == -1) { throw std::runtime_error("Reserved bit depth"); }
-    if (result < 1 || result > 32) { throw std::logic_error("Assertion error"); }
+    auto tresult = search_second(BIT_DEPTH_CODES, code);
+    if (!tresult.has_value()) { throw DataFormatException("Reserved bit depth"); }
+    auto result = tresult.value();
+    if (result < 1 || result > 32) { throw std::runtime_error("Assertion error"); }
     return result;
   }
 }
@@ -168,17 +180,21 @@ uint8_t FrameInfo::get_block_size_code(uint32_t block_size)
   } else if (1 <= block_size && block_size <= 65536) {
     result = 7;
   } else {
-    throw std::invalid_argument("Block size argument is invalid");
+    const std::string msg{ "block_size= " + std::to_string(block_size) + ", is not in the required ranges" };
+    throw std::invalid_argument(msg);
   }
 
-  if ((result >> 4U) != 0) { throw std::logic_error("Assertion error"); }
+  if ((result >> 4U) != 0) { throw std::runtime_error("Assertion error"); }
 
   return result;
 }
 
 uint8_t FrameInfo::get_sample_rate_code(uint32_t sample_rate)
 {
-  if (sample_rate == 0) { throw std::invalid_argument("Sample rate argument is invalid"); }
+  if (sample_rate == 0) {
+    const std::string msg{ "sample_rate= " + std::to_string(sample_rate) + ", is equal to zero" };
+    throw std::invalid_argument(msg);
+  }
 
   auto tresult = search_first(SAMPLE_RATE_CODES, sample_rate);
   uint8_t result = 0;
@@ -194,14 +210,17 @@ uint8_t FrameInfo::get_sample_rate_code(uint32_t sample_rate)
     result = 0;
   }
 
-  if ((result >> 4U) != 0) { throw std::logic_error("Assertion error"); }
+  if ((result >> 4U) != 0) { throw std::runtime_error("Assertion error"); }
 
   return result;
 }
 
 uint8_t FrameInfo::get_bit_depth_code(uint16_t bit_depth)
 {
-  if (bit_depth < 1 || bit_depth > 32) { throw std::invalid_argument("Bit depth is not valid"); }
+  if (bit_depth < 1 || bit_depth > 32) {
+    const std::string msg{ "bit_depth= " + std::to_string(bit_depth) + ", is less than 1 OR greater than 32" };
+    throw std::invalid_argument(msg);
+  }
 
   auto tresult = search_first(BIT_DEPTH_CODES, bit_depth);
   uint8_t result = 0;
@@ -211,7 +230,7 @@ uint8_t FrameInfo::get_bit_depth_code(uint16_t bit_depth)
     result = tresult.value();
   }
 
-  if ((result >> 3U) != 0) { throw std::logic_error("Assertion error"); }
+  if ((result >> 3U) != 0) { throw std::runtime_error("Assertion error"); }
 
   return result;
 }
